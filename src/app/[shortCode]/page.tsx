e8b86fbd1a4fb9701053df6922da10c1de6f7361
riskibarqy <riskibarqy@gmail.com>
2025-06-19 13:41:34 +0700

    fix : short code tsx

--- src/app/[shortCode]/page.tsx
+++ src/app/[shortCode]/page.tsx
@@ -1,23 +1,23 @@
-// src/app/[shortCode]/page.tsx
+// // src/app/[shortCode]/page.tsx
 
-import { redirect } from 'next/navigation';
+// import { redirect } from 'next/navigation';
 
-export default async function RedirectPage({
-  params,
-}: {
-  params: { shortCode: string };
-}) {
-  const { shortCode } = params;
+// export default async function RedirectPage({
+//   params,
+// }: {
+//   params: { shortCode: string };
+// }) {
+//   const { shortCode } = params;
 
-  const res = await fetch(`${process.env.API_URL}/urls/${shortCode}`, {
-    cache: 'no-store',
-  });
+//   const res = await fetch(`${process.env.API_URL}/urls/${shortCode}`, {
+//     cache: 'no-store',
+//   });
 
-  if (!res.ok) return <div>URL not found</div>;
+//   if (!res.ok) return <div>URL not found</div>;
 
-  const data = await res.json();
+//   const data = await res.json();
 
-  if (typeof data.url !== 'string') return <div>Invalid URL</div>;
+//   if (typeof data.url !== 'string') return <div>Invalid URL</div>;
 
-  redirect(data.url);
-}
+//   redirect(data.url);
+// }
